32bb308e722c374f27b591cf94fd0e5ff3ccfa4b
shiela-fajardo <shiela.fajardo.15@gmail.com>
2023-12-27 14:42:40 +0000

    12.4 2nd try

--- srv/travel-service.js
+++ srv/travel-service.js
@@ -9,22 +9,38 @@ init() {
   const { Travel, Booking, BookingSupplement } = this.entities
 
 
-   /**
+/**
    * Function import handler: getBookingDataOfPassenger
    * @param CustomerID
    * @returns BookingData
    */
-   this.on('getBookingDataOfPassenger', async (req) => {
-    const { CustomerID } = req.data
-    const allCustomerBookings = await SELECT `BookingStatus_code as status`.from (Booking) .where `to_Customer_CustomerID = ${CustomerID}`
-    const bookingData = {
-      HasNewBookings: false
+this.on('getBookingDataOfPassenger', async (req) => {
+  const { CustomerID } = req.data
+  const allCustomerBookings = await SELECT `BookingStatus_code as status`.from (Booking) .where `to_Customer_CustomerID = ${CustomerID}`
+  const bookingData = {
+    TotalBookingsCount: 0,
+    NewBookingsCount: 0,
+    AcceptedBookingsCount: 0,
+    CancelledBookingsCount: 0
+  }
+  allCustomerBookings.forEach((booking) => {
+    bookingData.TotalBookingsCount++
+    switch (booking.status) {
+      case 'N':
+        bookingData.NewBookingsCount++
+        break
+      case 'B':
+        bookingData.AcceptedBookingsCount++
+        break          
+      case 'X':
+        bookingData.CancelledBookingsCount++
+        break
+      default:
+        break
     }
-    bookingData.HasNewBookings = allCustomerBookings.some((booking) => {
-      return booking.status === 'N';
-    })
-    return bookingData
-  });
+  })
+  return bookingData;
+});
   
   /**
    * Fill in primary keys for new Travels.
